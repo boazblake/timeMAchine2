cd66d85dacae85aee530b0a95ea2f9fb5aa63cd5
boazblake <boazblake@gmail.com>
2016-03-21 23:14:07 -0500

    done

--- js/app.js
+++ js/app.js
@@ -39,7 +39,99 @@ import React, {Component} from 'react'
 function app() {
     // start app
     // new Router()
-    DOM.render(<p>test 2</p>, document.querySelector('.container'))
+
+    var yearData = {
+    	year:1985,
+    }
+
+    var AppView = React.createClass({
+    	render:function(){
+    		console.log(this)
+    		return(
+    			<div className="tmWrapper">
+    				<h1 className="heading">The Time Machine</h1>
+    				<TimeMachine yearData={this.props.yearData.year}/>
+    			</div>
+    		)
+    	}
+    })
+
+
+
+    var TimeMachine = React.createClass({
+
+
+    	_goFwd:function(){
+    		console.log(this)
+    		if(!this.state.ticking) {
+    			var incrementYear = function() {
+    				this.setState({
+    					year:this.state.year + 1,
+    					ticking: true,
+    					fwdButt: 'PAUSE'
+    				})
+    			}
+    			var boundIncrement = incrementYear.bind(this)
+    			this.intervalId = setInterval(boundIncrement, 500)
+    		}
+    		else {
+    			clearInterval(this.intervalId)
+    			this.setState({
+    				ticking:false,
+    				fwdButt:'... the future'
+    			})
+    		}
+    	},
+
+    	_goBack:function(){
+    		console.log(this)
+    		if(!this.state.ticking) {
+    			var decrementYear = function(){
+    				this.setState({
+    					year:this.state.year - 1,
+    					ticking:true,
+    					pastButt:'PAUSE'
+    				})
+    			}
+    			var boundRender = decrementYear.bind(this)
+    			this.intervalId = setInterval(boundRender, 500)
+    		}
+    		else {
+    			clearInterval(this.intervalId)
+    			this.setState({
+    				ticking:false,
+    				pastButt:'Back to ...'
+    			})
+    		}
+    	},
+
+
+
+    	getInitialState:function() {
+    		return{
+    			year:parseInt(this.props.yearData),
+    			fwdButt: '... the future',
+    			pastButt: 'Back to ...',
+    			ticking:false
+    		}
+    	},
+
+    	render: function(){
+    		console.log(this)
+    		return (
+    			<div className="yearWrapper">
+    				<div className="buttonWrapper">
+    					<button className="past buttons" onClick={this._goBack}>{this.state.pastButt}</button>
+    					<button className="future buttons" onClick={this._goFwd}>{this.state.fwdButt}</button>
+    				</div>
+    					<h2 className="year" key="year">{this.state.year}</h2>
+    			</div>
+    		)
+    	}
+    })
+
+
+    DOM.render(<AppView yearData={yearData}/>, document.querySelector('.container'))
 }
 
 app()
